63c0292a894313aae3ab936f6013e98ec7fa3b93
Anna Gruber <anna@schwindelfuchs.de>
2013-06-26 16:35:01 +0200

    improved game

--- game.js
+++ game.js
@@ -6,118 +6,131 @@
  * To change this template use File | Settings | File Templates.
  */
 
-var outerServer;
-var outerGameEngine;
 
-define(["server", "jquery"], function(server){
-    outerServer = server;
-var gameEngine = {
-    playerInputPositions: [0, 0],
-    player0: {ready: false, elementId: "player0", id : 0},
-    player1: {ready: false, elementId: "player1", id : 1},
-    ball: {x: 2, y: 2, elementId: "ball"},
-    ready: false,
-    countDown: 3,
+define(["server", "jquery"], function (server) {
 
-    startGame: function(){
-        $(".gameState").html(gameEngine.countDown);
-        var intervalId = window.setInterval(function(){
-            gameEngine.countDown--;
-            if(gameEngine.countDown == 0){
-                $(".gameState").html("");
-                gameEngine.ready = true;
-                window.clearInterval(intervalId);
-            }else {
+    var gameEngine = {
+        $playGround: null,
+        playerInputPositions: [0, 0],
+        player0: {elementId: "player0", id: 0},
+        player1: {elementId: "player1", id: 1},
+        ball: {x: 3, y: 3, $ball: null},
+        ready: false,
+        countDown: 3,
+
+        init: function () {
+            this.$playGround = $("#playground");
+            this.ball.$ball = $("#ball");
+            var randoms = [-3, -2, 2, 3];
+            this.ball.x = randoms[parseInt(Math.random() * 4)];
+            this.ball.y = randoms[parseInt(Math.random() * 4)];
+        },
+
+        startGame: function () {
+            if (!this.ready) {
+                gameEngine.countDown = 3;
+                this.ball.$ball.show();
+                this.ball.$ball.css({"top": "50%", "left": "50%"});
+                var randoms = [-3, -2, 2, 3];
+                this.ball.x = randoms[parseInt(Math.random() * 4)];
+                this.ball.y = randoms[parseInt(Math.random() * 4)];
                 $(".gameState").html(gameEngine.countDown);
+                var intervalId = window.setInterval(function () {
+                    gameEngine.countDown--;
+                    if (gameEngine.countDown == 0) {
+                        $(".gameState").html("");
+                        gameEngine.ready = true;
+                        window.clearInterval(intervalId);
+                    } else {
+                        $(".gameState").html(gameEngine.countDown);
+                    }
+
+                }, 1000);
             }
+            // this.ready = true;
+        },
 
-        }, 1000);
-        // this.ready = true;
-    },
-
-    movePaddles: function () {
-        var $playGround = $("#playground");
-        var $player0 = $("#" + this.player0.elementId);
-        var $player1 = $("#" + this.player1.elementId);
-        this.movePaddle($player0, $playGround, this.playerInputPositions[0]);
-        this.movePaddle($player1, $playGround, this.playerInputPositions[1]);
-    },
-    movePaddle: function ($paddle, $playGround, additionalPos) {
-        var position = $paddle.position();
-        var offset = additionalPos * 7;
-        if (position.top + offset <= $playGround.height() - $paddle.height() && position.top + offset >= 0) {
-            $paddle.css("top", (position.top + offset) + "px");
-        }
-    },
-    collision: function ($player, position, $ball, $playGround) {
-        var pBottom = $player.position().top;
-        var pTop = pBottom + $player.height();
-        var center = position.top + ($ball.height() / 2);
-        if (center > pBottom && pTop > center) {
-            this.ball.y += parseInt(Math.random() * 5) - 3;
-            this.ball.x = (-1) * this.ball.x;
-            return true;
-        }
-        else {
-            $(".gameState").html("Game Over");
-            $ball.hide();
-            return false;
-        }
-    }, moveBall: function () {
-        if (this.ready) {
-            var $playGround = $("#playground");
-            var $ball = $("#" + this.ball.elementId);
-            var position = $ball.position();
-
-            if (position.top + this.ball.y < 0 || position.top + this.ball.y > $playGround.height() - $ball.height()) {
-                this.ball.y = (-1) * this.ball.y;
-            }
-            var newY = position.top + this.ball.y;
-
+        movePaddles: function () {
             var $player0 = $("#" + this.player0.elementId);
             var $player1 = $("#" + this.player1.elementId);
+            this.movePaddle($player0, this.playerInputPositions[0]);
+            this.movePaddle($player1, this.playerInputPositions[1]);
+        },
+        movePaddle: function ($paddle, additionalPos) {
+            var position = $paddle.position();
+            var offset = additionalPos * 7;
+            if (position.top + offset <= this.$playGround.height() - $paddle.height() && position.top + offset >= 0) {
+                $paddle.css("top", (position.top + offset) + "px");
+            }
+        },
+        collision: function ($player, position) {
+            var pBottom = $player.position().top;
+            var pTop = pBottom + $player.height();
+            var center = position.top + (this.ball.$ball.height() / 2);
+            if (center > pBottom && pTop > center) {
+                this.ball.y += parseInt(Math.random() * 2);
+                this.ball.x = (-1) * this.ball.x;
+                return true;
+            }
+            else {
+                $(".gameState").html("Game Over");
+                this.ball.$ball.hide();
+                return false;
+            }
+        }, moveBall: function () {
+            if (this.ready) {
 
-            if (position.left + this.ball.x <= $player0.width() + 5) {
-                this.ready = this.collision($player0, position, $ball, $playGround);
-                if(!this.ready){
-                    server.postGameOver(player1.id)
+
+                var position = this.ball.$ball.position();
+
+                if (position.top + this.ball.y < 0 || position.top + this.ball.y > this.$playGround.height() - this.ball.$ball.height()) {
+                    this.ball.y = (-1) * this.ball.y;
                 }
+                var newY = position.top + this.ball.y;
+
+                var $player0 = $("#" + this.player0.elementId);
+                var $player1 = $("#" + this.player1.elementId);
+
+                if (position.left + this.ball.x <= $player0.width() + 5) {
+                    this.ready = this.collision($player0, position);
+                    if (!this.ready) {
+                        server.postGameOver(player1.id)
+                    }
+                }
+
+                if (position.left + this.ball.x > this.$playGround.width() - this.ball.$ball.width() - $player1.width() - 5) {
+                    this.ready = this.collision($player1, position);
+                    if (!this.ready) {
+                        server.postGameOver(player0.id)
+                    }
+                }
+
+                var newX = position.left + this.ball.x;
+
+                this.ball.$ball.css({"left": newX + "px", "top": newY + "px"});
             }
 
-            if (position.left + this.ball.x > $playGround.width() - $ball.width() - $player1.width() - 5) {
-                this.ready = this.collision($player1, position, $ball, $playGround);
-                if(!this.ready){
-                    server.postGameOver(player0.id)
-                }
-            }
 
-            var newX = position.left + this.ball.x;
-
-            $ball.css({"left": newX + "px", "top": newY + "px"});
         }
 
 
-    }
+    };
+
+    window.requestAnimationFrame(function move() {
+        gameEngine.movePaddles();
+        gameEngine.moveBall();
+        window.requestAnimationFrame(move);
+
+    });
 
 
-};
-    outerGameEngine = gameEngine;
+    server.onPosition(function (player, position) {
+        console.log("got new position: ", player, position);
+        gameEngine.playerInputPositions[player] = position;
+    });
 
-window.requestAnimationFrame(function move() {
-    gameEngine.movePaddles();
-    gameEngine.moveBall();
-    window.requestAnimationFrame(move);
-
-});
-
-
-server.onPosition(function (player, position) {
-    console.log("got new position: ", player, position);
-    gameEngine.playerInputPositions[player] = position;
-});
-
-server.onReady(function () {
-    console.log("on ready: ");
-    gameEngine.startGame();
-});
+    server.onReady(function () {
+        console.log("on ready: ");
+        gameEngine.startGame();
+    });
 });
