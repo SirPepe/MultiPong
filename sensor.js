86e23caef73ea95cbffee841cb82a25f4d758064
Mehmed Mert <mmert1988@googlemail.com>
2013-06-26 14:11:54 +0200

    sensor.js mit define

--- sensor.js
+++ sensor.js
@@ -1,36 +1,40 @@
-var info = document.getElementById('info');
+define(["server"], function(server){
 
-window.addEventListener('deviceorientation', function(evt){
-	var position = (evt.beta / 90);
+	var info = document.getElementById('info');
 
-	// debug info ausgeben
-	var msg = 
-	"Vorwärts-Rückwärts: " + position;
-	info.innerHTML = msg;
-	
-	// TODO Position an Server übermitteln
-	server.postPosition(position);
-	
-}, false);
+	window.addEventListener('deviceorientation', function(evt){
+		var position = (evt.beta / 90);
+
+		// debug info ausgeben
+		var msg = 
+		"Vorwärts-Rückwärts: " + position;
+		info.innerHTML = msg;
+		
+		// TODO Position an Server übermitteln
+		server.postPosition(position);
+		
+	}, false);
 
 
-var readyBtn = document.getElementById('readyBtn');
+	var readyBtn = document.getElementById('readyBtn');
 
-readyBtn.onclick = function() {
-	// TODO Ready an Server schicken
-	server.postReady();
-	this.style.visibility = 'hidden';
-};
+	readyBtn.onclick = function() {
+		// TODO Ready an Server schicken
+		server.postReady();
+		this.style.visibility = 'hidden';
+	};
 
-var onGameOverFtn = function onGameOver(win) {
-	if (win) {
-		document.body.style.backgroundColor = 'rgb(0, 255, 0)';
-		document.getElementById('win').innerHTML = 'Gewonnen';
-	} else {
-		document.body.style.backgroundColor = 'rgb(255, 0, 0)';
-		document.getElementById('win').innerHTML = 'Verloren';
-	}
-};
+	var onGameOverFtn = function onGameOver(win) {
+		if (win) {
+			document.body.style.backgroundColor = 'rgb(0, 255, 0)';
+			document.getElementById('win').innerHTML = 'Gewonnen';
+		} else {
+			document.body.style.backgroundColor = 'rgb(255, 0, 0)';
+			document.getElementById('win').innerHTML = 'Verloren';
+		}
+	};
 
-server.onGameOver(onGameOverFtn);
+	server.onGameOver(onGameOverFtn);
+
+});
 
