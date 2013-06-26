900a4e2d74c3f6072ff370554d1b37ced7646fb8
Mehmed Mert <mmert1988@googlemail.com>
2013-06-26 16:15:52 +0200

    fix jquery click

--- sensor.js
+++ sensor.js
@@ -22,7 +22,7 @@ define(["server", "jquery"], function(server){
 		// debug info ausgeben
 		var msg = 
 		"Vorwaerts-Rueckwaerts: " + position;
-		$info.innerHTML = msg;
+		$info.html(msg);
 		
 		// Position an Server uebermitteln
 		server.postPosition(position);
@@ -32,11 +32,11 @@ define(["server", "jquery"], function(server){
 
 	var $readyBtn = $('#readyBtn');
 
-	$readyBtn.onclick = function() {
+	$readyBtn.click(function() {
 		// Ready an Server schicken
 		server.postReady();
 		this.style.visibility = 'hidden';
-	};
+	});
 
 	var onGameOverFtn = function onGameOver(win) {
 		if (win) {
